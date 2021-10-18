1f809018b6710442048da2870615a00865823e43
fedex100 <miguez.35@gmail.com>
2021-10-18 21:33:07 +0000

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/te/oDataSalesOrderSearch/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.te.oDataSalesOrderSearch.controller.MainView", {});
+});
